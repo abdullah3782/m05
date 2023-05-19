d13e08ace9bb81a0011960b58b51292f877b98fb
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2023-05-19 10:35:06 +0200

    Agregado archivo admin.java

---
+++ admin.java
@@ -0,0 +1,10 @@
+class Admin {
+    static boolean enroll(String course, String student) {
+      if (Data.students.contains(student) && Data.courses.contains(course)) {
+        Data.enrollments.add(new String[] { student, course });
+      }
+      return false;
+    }
+   }
+   
+   
